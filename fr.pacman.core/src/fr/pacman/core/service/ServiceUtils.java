9fe62ac469f6ba2929a7571ea531f1e114c23e89
pg1971 <patrice.garaud@gmail.com>
2025-03-04 18:22:42 +0100

    A nouveau retour arrière sur nom dto <> nom service + mise en place génération crud + première modification des mappers pour préparer lazyloading.

--- fr.pacman.core/src/fr/pacman/core/service/ServiceUtils.java
+++ fr.pacman.core/src/fr/pacman/core/service/ServiceUtils.java
@@ -1,12 +1,10 @@
 package fr.pacman.core.service;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.obeonetwork.dsl.environment.DTO;
 import org.obeonetwork.dsl.environment.Reference;
-import org.obeonetwork.dsl.soa.Service;
 
 /**
  * 
@@ -20,23 +18,15 @@ public class ServiceUtils {
 	 * @param p_services
 	 * @return
 	 */
-	public static Set<DTO> get_associatedProvider(Set<DTO> p_associatedDtos, List<Service> p_services) {
+	public static Set<DTO> get_associatedProvider(Set<DTO> p_associatedDtos) {
 		Set<DTO> dtos = new HashSet<>();
-		DTO dto;
-
 		for (DTO associatedDto : p_associatedDtos) {
 			dtos.add(associatedDto);
 			for (Reference reference : associatedDto.getReferences()) {
 				if (null != reference.getOppositeOf()) {
-					dto = (DTO) reference.getOppositeOf().getContainingType();
+					dtos.add((DTO) reference.getOppositeOf().getContainingType());
 				} else {
-					dto = (DTO) reference.getReferencedType();
-				}
-				for (Service service : p_services) {
-					if (service.getName().equalsIgnoreCase(dto.getName())) {
-						dtos.add(dto);
-						break;
-					}
+					dtos.add((DTO) reference.getReferencedType());
 				}
 			}
 		}
