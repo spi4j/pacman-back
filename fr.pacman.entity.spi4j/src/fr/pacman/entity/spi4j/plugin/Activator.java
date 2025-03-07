33e1e8b812031761059f38732adcbed325340f79
pg1971 <patrice.garaud@gmail.com>
2025-03-07 19:27:04 +0100

    Modifications diverses + mise en places des plugins requirement.

--- fr.pacman.entity.spi4j/src/fr/pacman/entity/spi4j/plugin/Activator.java
+++ fr.pacman.entity.spi4j/src/fr/pacman/entity/spi4j/plugin/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends Plugin {
 
-	public static final String c_pluginId = "fr.pacman.entity";
+	public static final String c_pluginId = "fr.pacman.entity.spi4j";
 	private static Activator plugin;
 
 	@Override
