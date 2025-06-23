dd90a18a5a6f41cecda44800677262038b84c98a
Patrice GARAUD <patrice.garaud@fortil.group>
2025-06-23 07:05:28 +0200

    Corrections coquilles + ajout relations + ajout suppression cache.

--- script-html-wiki.py
+++ script-html-wiki.py
@@ -210,6 +210,9 @@ def generate_page(title, sidebar_html, toc_html, body_html):
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 <title>{title}</title>
 <style>
  body {{ margin:0; font-family: Arial,sans-serif; display: block; overflow-x: hidden;}}
